14bddc04f2708d019c40fef0119e8eab70847165
Hornsby Bathroom <hornsbybathroom@Snow-Leopards-MacBook-Pro.local>
2012-04-12 00:50:51 +1000

    New hornsbybathroom app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hornsbybathroom::Application.config.secret_token = '89bafec9d648f754474458a486ab6b55130265bac50a861bcbf1462f731af92e5355dba9e138b83ff9b6bf8cc645215e16905d270045af36df3436ea7ec8f4d7'
