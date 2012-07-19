64f5c0cc9218808cab2478f06c41caeebc4fc8e2
Hornsby Bathroom <hornsbybathroom@gmail.com>
2012-07-20 00:49:27 +1000

    Temp excluded users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../seed/users', __FILE__)
+require File.expand_path('../seed/users', __FILE__) if Rails.env.development?
 require File.expand_path('../seed/locations', __FILE__)
 require File.expand_path('../seed/testimonials', __FILE__) if Rails.env.development?
 
