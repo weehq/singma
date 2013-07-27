7f0030e6a0f5d939c5d2ece05411c316a20821f7
Wee HQ <weehq@Snow-Leopards-MacBook-Pro.local>
2013-07-27 22:01:05 +1000

    Delete unit tests

--- test/functional/quote_mailer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class QuoteMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/site_map_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SiteMapHelperTest < ActionView::TestCase
-end
