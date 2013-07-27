b9490100fd8572e2eff8a18000aed21fd25e34a6
Eric Wee <wee007@gmail.com>
2013-07-27 22:25:50 +1000

    Add multiline param on postcode and phone validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,16 @@
 class Contact < ActiveRecord::Base
   # Validations
   validates :name, :length => {:minimum => 2}
-  validates :postcode, :numericality => true, :length => {:minimum => 4}, :format => { :with => /^2.+/, :message => 'must start with 2' }
+  validates :postcode, :numericality => true,
+                       :length => {:minimum => 4},
+                       :format => {
+                         :with => /^2.+/,
+                         :message => 'must start with 2',
+                         :multiline => true
+                       }
   validates :email, :presence => true
-  validates :phone, :numericality => true, :length => {:minimum => 8}
+  validates :phone, :numericality => true,
+                    :length => {:minimum => 8}
   validates :prefer_contact, :presence => true
   validates :bathroom_requirements, :presence => true
 
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,8 +2,15 @@ class Quote < ActiveRecord::Base
   # Validations
   validates :name, :length => {:minimum => 2}
   validates :email, :presence => true
-  validates :phone, :numericality => true, :length => {:minimum => 8}
-  validates :postcode, :numericality => true, :length => {:minimum => 4}, :format => { :with => /^2.+/, :message => 'must start with 2' }
+  validates :phone, :numericality => true,
+                    :length => {:minimum => 8}
+  validates :postcode, :numericality => true,
+                       :length => {:minimum => 4},
+                       :format => {
+                         :with => /^2.+/,
+                         :message => 'must start with 2',
+                         :multiline => true
+                       }
   validate :postcode_invalidity
   validates :access_difficulty, :presence => true
   # validates_date :job_completion, :after => :today
