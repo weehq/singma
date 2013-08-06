955115587a2ba3712fcf94f330a448b1982e339c
Eric Wee <wee007@gmail.com>
2013-08-06 22:44:27 +1000

    Add more admin users

--- db/seed/users.rb
+++ db/seed/users.rb
@@ -5,8 +5,10 @@ module Users
   User.delete_all
 
   puts "Loading users ...\n"
-  User.create(username: 'ericwee', password: 'weeEric2012', email: 'wee007@gmail.com')
-  User.create(username: 'kenlai', password: 'laiKen2012', email: 'hbathrooms@gmail.com')
+  User.create(username: 'admin', password: 'admin2013', email: 'admin@singma.com.au')
+  User.create(username: 'ericwee', password: 'weeEric2013', email: 'wee007@gmail.com')
+  User.create(username: 'tonywee', password: 'weeTony2013', email: 'tonywee@gmail.com')
+  User.create(username: 'kenlai', password: 'laiKen2013', email: 'hbathrooms@gmail.com')
 
   puts "[END] Users\n\n"
 end
