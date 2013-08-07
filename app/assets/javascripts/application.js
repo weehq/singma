928c072124e5871c24fdf335c3eeb784087c9dac
Tony Wee <tonywee88@gmail.com>
2013-08-07 21:21:17 +1000

    Fixed Nav Bar, jQuery Form Validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,7 +23,159 @@ jQuery(function($) {
       transition: 'fade',
       responsive: true
     });
-  }
+  };
+
+  jQuery.validator.addMethod("postcode", function(value, element) {
+      return this.optional(element) || /^2/.test(value);
+    },
+    "Postcode must start with 2"
+  );
+
+  $('#new_quote').validate({
+    rules: {
+      'quote[name]': {
+        required: true,
+        minlength: 2
+      },
+      'quote[email]': {
+        required: true,
+        email: true
+      },
+      'quote[phone]': {
+        required: true,
+        digits: true,
+        minlength: 8
+      },
+      'quote[postcode]': {
+        required: true,
+        digits: true,
+        minlength: 4,
+        postcode: true
+      },
+      'quote[access_difficulty]': {
+        required: true
+      },
+    },
+    messages: {
+      'quote[name]': {
+        required: "Please enter a name",
+        minlength: "Name is too short (minimum is 2 characters)"
+      },
+      'quote[email]': {
+        required: "Please enter a valid email address",
+        email: "Your email address must be in the format of name@domain.com"
+      },
+      'quote[phone]': {
+        required: "Please enter a phone number",
+        digits: "Phone must be a number",
+        minlength: "Phone is too short (minimum is 8 characters)"
+      },
+      'quote[postcode]': {
+        required: "Please enter a valid postcode",
+        digits: "Postcode must be a number",
+        minlength: "Postcode is too short (must be 4 characters)"
+      },
+      'quote[access_difficulty]': {
+        required: "Access difficulty can't be blank"
+      },
+    },
+    invalidHandler: function(event, validator) {
+      jQuery('.submit_button.process').val('Submit');
+    }
+  });
+
+  $('#new_contact').validate({
+    rules: {
+      'contact[name]': {
+        required: true,
+        minlength: 2
+      },
+      'contact[postcode]': {
+        required: true,
+        digits: true,
+        minlength: 4,
+        postcode: true
+      },
+      'contact[phone]': {
+        required: true,
+        digits: true,
+        minlength: 8
+      },
+      'contact[email]': {
+        required: true,
+        email: true
+      },
+      'contact[prefer_contact]': {
+        required: true
+      },
+      'contact[bathroom_requirements]': {
+        required: true
+      },
+      'contact[budget]': {
+        number: true
+      },
+    },
+    messages: {
+      'contact[name]': {
+        required: "Please enter a name",
+        minlength: "Name is too short (minimum is 2 characters)"
+      },
+      'contact[postcode]': {
+        required: "Please enter a valid postcode",
+        digits: "Postcode must be a number",
+        minlength: "Postcode is too short (must be 4 characters)"
+      },
+      'contact[phone]': {
+        required: "Please enter a phone number",
+        digits: "Phone must be a number",
+        minlength: "Phone is too short (minimum is 8 characters)"
+      },
+      'contact[email]': {
+        required: "Please enter a valid email address",
+        email: "Your email address must be in the format of name@domain.com"
+      },
+      'contact[prefer_contact]': {
+        required: "Prefer contact can't be blank"
+      },
+      'contact[bathroom_requirements]': {
+        required: "Bathroom requirements can't be blank"
+      },
+      'contact[budget]': {
+        number: "Budget must be a number"
+      },
+    },
+    invalidHandler: function(event, validator) {
+      jQuery('.submit_button.process').val('Submit');
+    }
+  });
+
+  $('#new_testimonial').validate({
+    rules: {
+      'testimonial[name]': {
+        required: true
+      },
+      'testimonial[suburb]': {
+        required: true
+      },
+      'testimonial[comment]': {
+        required: true
+      },
+    },
+    messages: {
+      'testimonial[name]': {
+        required: "Please enter a name",
+      },
+      'testimonial[suburb]': {
+        required: "Please enter a suburb",
+      },
+      'testimonial[comment]': {
+        required: "Please enter a comment",
+      },
+    },
+    invalidHandler: function(event, validator) {
+      jQuery('.submit_button.process').val('Submit');
+    }
+  });
 
   // Refresh recent testimonial every 2 minutes
   setInterval(function() {
